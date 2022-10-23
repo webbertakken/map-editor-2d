1444405c1c9231b5d9c83cd41a3ba38e901efeee
Webber <webber@takken.io>
2022-10-23 03:53:26 +0200

    feat: add canvas

---
+++ src/components/canvas/Canvas.tsx
@@ -0,0 +1,15 @@
+import { Stage, Layer, Rect, Circle } from 'react-konva';
+
+export const Canvas = () => {
+  return (
+    // Stage - is a div wrapper
+    // Layer - is an actual 2d canvas element, so you can have several layers inside the stage
+    // Rect and Circle are not DOM elements. They are 2d shapes on canvas
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Rect width={50} height={50} fill="red" />
+        <Circle x={200} y={200} stroke="black" radius={50} />
+      </Layer>
+    </Stage>
+  );
+}
