ff27bc97a86e9e16b52bf36865df9b6bb45ec189
Webber <webber@takken.io>
2022-10-28 23:07:05 +0200

    fix: make global hotkeys less aggressive

--- src/service/HotkeyNotifier.tsx
+++ src/service/HotkeyNotifier.tsx
@@ -1,18 +1,29 @@
 import { register, unregisterAll } from '@tauri-apps/api/globalShortcut'
 import { v4 as uuidv4 } from 'uuid'
 import { createObservable, Observable } from '../core/observable'
+import { listen, TauriEvent } from '@tauri-apps/api/event'
 
 export class HotkeyNotifier {
-  registry: Map<string, Observable>
+  events: Map<string, Observable>
 
   constructor() {
-    this.registry = new Map<string, Observable>()
-    this.registry.set('copy', createObservable())
-    this.registry.set('paste', createObservable())
-    this.registry.set('escape', createObservable())
+    this.events = new Map<string, Observable>()
+    this.events.set('copy', createObservable())
+    this.events.set('paste', createObservable())
+    this.events.set('escape', createObservable())
   }
 
   public async registerListeners() {
+    // Register
+    await listen(TauriEvent.WINDOW_FOCUS, this.register.bind(this))
+
+    // Unregister
+    await listen(TauriEvent.WINDOW_BLUR, this.unregister.bind(this))
+    await listen(TauriEvent.MENU, this.unregister.bind(this))
+  }
+
+  private async register() {
+    console.log('registering hotkeys')
     await unregisterAll()
     await register('CommandOrControl+C', () => {
       this.copy?.notify(uuidv4())
@@ -25,15 +36,20 @@ export class HotkeyNotifier {
     })
   }
 
+  private async unregister() {
+    console.log('unregistering hotkeys')
+    await unregisterAll()
+  }
+
   public get copy(): Observable | undefined {
-    return this.registry.get('copy')
+    return this.events.get('copy')
   }
 
   public get paste(): Observable | undefined {
-    return this.registry.get('paste')
+    return this.events.get('paste')
   }
 
   public get escape(): Observable | undefined {
-    return this.registry.get('escape')
+    return this.events.get('escape')
   }
 }
