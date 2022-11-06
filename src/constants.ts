584ba2ceaf4f9f7cf3bde100e7075fae2266c756
Webber <webber@takken.io>
2022-11-06 22:18:03 +0100

    fix: rotation and scaled rotation

--- src/constants.ts
+++ src/constants.ts
@@ -12,10 +12,3 @@ export const REGEX_TRAILING_SLASHES = /[\\/]+$/
 export const SCENE_FILE_CURRENT_VERSION = '0.0.1'
 export const SCENE_FILE_TYPE_NAME = '2D Map Editor Scene' // or when standardised: 2D Transmission Format
 export const SCENE_FILE_TYPE_EXTENSION = '2dtf'
-
-/**
- * Canvas
- */
-
-// Note: use with caution, this may not work well in combination with snapping.
-export const CANVAS_LIFT_UP_SCALING_FACTOR = 1.0
